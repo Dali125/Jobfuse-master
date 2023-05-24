0c0c2d92dfa5404d3650ff373d9918a1126ac35b
Dali125 <dalitsongulube@gmail.com>
2023-05-24 08:25:35 +0200

    addingexpansive widget

--- lib/ui/components/posts_page/services/servicesTab.dart
+++ lib/ui/components/posts_page/services/servicesTab.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SelecteServices extends StatefulWidget {
@@ -12,12 +13,73 @@ class SelecteServices extends StatefulWidget {
 class _SelecteServicesState extends State<SelecteServices> {
   @override
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.category),
 
                   ),
-    body:Center(child: Text('hsrh'),)
+    body:  SizedBox(
+      height: 500,
+      width: width,
+      child: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('freelance_services')
+            .where('category', isEqualTo: widget.category)
+            .snapshots(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+              scrollDirection: Axis.vertical,
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var services = snapshot.data!.docs[index];
+                return GestureDetector(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Material(
+                      elevation: 10,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 250,
+                            width: width,
+                            child: Image.network(
+                              services['service_image'],
+                              height: 110,
+                              fit: BoxFit.fitWidth,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(services['description']),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
+                    ),
+                  ),
+                  onTap: (){
+
+                    print('object');
+                  },
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 40);
+              },
+            );
+          } else if (snapshot.hasError) {
+            return const Icon(Icons.error_outline);
+          } else {
+            return const CircularProgressIndicator();
+          }
+        },
+      ),
+    )
     );
   }
 }
